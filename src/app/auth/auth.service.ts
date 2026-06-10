6e8a473c5fad832a864ab86eefab0b35f4ea30e5
Ngueyep Alexandra <alexandra.ngueyep@saintjeaningenieur.org>
2026-06-10 10:49:05 +0100

    Fix: Utilisation de l'URL de production pour l'API

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(
     private http   : HttpClient,
