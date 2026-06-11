b759e3d4133bf1ea37d34254b65401542cca4545
Ngueyep Alexandra <alexandra.ngueyep@saintjeaningenieur.org>
2026-06-11 19:38:29 +0100

    modifica

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,4 @@
-// Détection automatique de l'environnement
-const isLocal = window.location.hostname === 'localhost' || 
-                window.location.hostname === '127.0.0.1';
-
 export const environment = {
-  production: !isLocal,
-  apiUrl: isLocal 
-    ? 'http://127.0.0.1:8000/api' 
-    : 'https://mon-backend-django.onrender.com/api'
+  production: false,
+  apiUrl: 'http://127.0.0.1:8000/api'
 };
---
+++ src/environments/environnement.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://mon-backend-django.onrender.com/api'
+};
